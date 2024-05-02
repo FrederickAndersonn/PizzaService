0cd83bf0d158f415fc9f9a731767d1b3a69a613c
FrederickAndersonn <frederickanderson71@yahoo.com>
2024-05-02 21:48:30 +0200

    put pizza service to git

--- README.md
+++ README.md
@@ -56,7 +56,7 @@ For normal access to the database without serious permissions please always use 
 
 ### PHPmyAdmin
 
-To access `phpmyadmin` go to [http://locadlhost:8085/](http://localhost:8085/). For convenience there is a forward from [http://localhost/phpmyadmin](http://localhost/phpmyadmin).
+To access `phpmyadmin` go to [http://localhost:8085/](http://localhost:8085/). For convenience there is a forward from [http://localhost/phpmyadmin](http://localhost/phpmyadmin).
 
 Use the credentials you have set in the `env.txt` file for login. The database will be stored persistently. But if you rebuild the database container your entries will be lost. So you should better export a modified database scheme into a sql-file. 
 Hint: The files in the folder `Docker\mariadb.setup` are read when building the docker containers and imported into the database. So if you want to make persistent changes this is the place to go. The files will be imported in alphabetic order. To see changes you made in the files you have to rebuild the MariaDB-container. Be aware that any error in those files will crash the startup of your MariaDB-container! 
@@ -84,6 +84,3 @@ Note: The name of this image may vary depending on your installation. Please che
   
 # EWA using XAMPP & Co.
 If you can not run docker you have to setup the environment for EWA yourself. You need the same setup that is used for the EWA-lab. You will find some hints on the installation in the section "Technische Hinweise" of the current EWA lab. 
-
-  Enjoy
-  Ralf Hahn, Feb 2022
--- src/index.php
+++ src/index.php
@@ -1,20 +1,5 @@
 <?php
 header("Content-type: text/html");
-
-$Prak="";
-$PrakInfo="";
-if (is_dir("Praktikum")) {
-    $PrakInfo="<li>Work on your lab exercises in the folder <b>Praktikum</b>.</li>";
-    $Prak = "<a class=\"flex-item\" href=\"./Praktikum\">Praktikum</a>";
-}
-
-$Exam="";
-$ExamInfo="";
-if (is_dir("Exam")) {
-    $ExamInfo="<li><strong>Use the folder Exam (located in src/Exam) for your solution of the examination. Do not use any other folder!</strong></li>";
-    $Exam = "<a class=\"flex-item flex-item--warning\" href=\"./Exam\">Klausur</a>";
-}
-
 echo <<<HTML
 <!DOCTYPE html>
 <html lang="de">
@@ -26,30 +11,11 @@ echo <<<HTML
 </head>
 <body>
 <header>
-    <h1>EWA Repository</h1>
+    <h1>Pizza_Service</h1>
 </header>
 <main>
-    <ul>
-        <li>This is the content of the file 'index.php' in the folder <b>'src'</b>!</li>
-        <li>For older examinations you may have a look in the <b>Klausuren</b> folder</li>
-        <li>Many good examples are located in <b>Demos</b></li>
-        <li>Play around in the <b>Playground</b> folder</li>
-        $PrakInfo
-        <li>Use the <b>Exam_Probe</b> folder for our exam test run</li>
-        $ExamInfo
-    </ul>
-    <h2>Navigation</h2>
     <div class="flex-container">
-        <a href="./Demos" class="flex-item">Demos</a>
-        <a href="./Klausuren" class="flex-item">Altklausuren</a>
-    </div>
-    <div class="flex-container">
-        $Prak
-        <a href="./Playground" class="flex-item">Spielwiese</a>
-    </div>
-    <div class="flex-container">
-        <a href="./Exam_Probe" class="flex-item">Probeklausur</a>
-        $Exam
+        <a href="./Pizza_Service" class="flex-item">Go To Web</a>
     </div>
 </main>
 </body>
